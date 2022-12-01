e46fe1dcca4f9f6f1d1648483a7e295ff24bd817
GustavoML <iscmedinalg@gmail.com>
2022-11-30 21:52:45 -0600

    Microservice Completed

--- index.js
+++ index.js
@@ -18,15 +18,48 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//call path with the middleware
+//Test 1
+app.get('/api/:date?', (request, response) => {
+  //Create object for add the unix and utc times
+  let responseObject = {}
+  //get the date
+  let date = request.params.date
+
+  //if the date is empty return utc and unix time
+  if(!date){
+      //Test 7
+      responseObject['unix'] = new Date().getTime()
+      //Test 8
+      responseObject['utc'] = new Date().toUTCString()
+  
+      response.json(responseObject)
+    }
+  if(date.includes('-') || date.includes(' ')){
+    //Test 1
+    responseObject['unix'] = new Date(date).getTime()
+    //Test 2
+    responseObject['utc'] = new Date(date).toUTCString()
+  }else{
+    //Test 3
+    date = parseInt(date)
+    responseObject['unix'] = new Date(date).getTime()
+    responseObject['utc'] = new Date(date).toUTCString()
+  }
+  //Test 4
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  
+  response.json(responseObject)
+})
